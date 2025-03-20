2ae3bfa4e67e62c794ad77c89b3b4135695f0cd3
Abanta6 <dalexbanta95@gmail.com>
2025-03-20 00:37:04 -0500

    App built, and functions created in ShowSelection and ShowDetails

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,22 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({
+  shows,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          className={"shows" + (show === selectedShow ? "selected" : "")}
+          key={shows.name}
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
